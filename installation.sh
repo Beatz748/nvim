d94009ddf660aad160202f6f09ad6b26fc41c28d
Beatz748 <pashkamos77@gmail.com>
2023-09-15 02:38:35 +0300

    NeoVim <3

--- installation.sh
+++ installation.sh
@@ -1,8 +1,13 @@
-export VUNDLE="${HOME}/.config/nvim/plugins/Vundle.vim"
+
+export VUNDLE="${HOME}/.config/nvim/plugins/Vundle.vim" # You can edit this line, if you already
+							# donwloaded Vundle, set another path.
+echo "Vundle will be downloaded to the $VUNDLE"
+
 
 git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE
 
-
+echo "Editing rtp for Vundle"
 echo "set rtp+=${VUNDLE}" | cat - init.vim > temp && mv temp init.vim
 
+echo "If you are using zshrc, you can type 'v' instead of 'nvim'"
 alias v=nvim >> $HOME/.zshrc
