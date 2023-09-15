34dc6004da580555540b6d1443ba8208ecac48d8
Beatz748 <pashkamos77@gmail.com>
2023-09-15 04:16:27 +0300

    NeoVim <3

--- installation.sh
+++ installation.sh
@@ -4,7 +4,7 @@ echo "Creating directory for nvim"
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/nvim
 
-export VUNDLE="${HOME}/.config/nvim/plugins/Vundle.vim" # You can edit this line, if you already
+export VUNDLE="${HOME}/.config/nvim/plugged/Vundle.vim" # You can edit this line, if you already
 							# donwloaded Vundle, set another path.
 echo "Vundle will be downloaded to the $VUNDLE"
 
@@ -17,4 +17,19 @@ echo "set rtp+=${VUNDLE}" | cat - init.vim > temp && mv temp init.vim
 echo "If you are using zshrc, you can type 'v' instead of 'nvim'"
 alias v=nvim >> $HOME/.zshrc
 
+echo "copy settings to default directory"
 cp coc-settings.json init.vim $HOME/.config/nvim/
+
+cd ..
+
+rm -rf nvim
+
+nvim -c ":PluginInstall" -c ":q" -c ":q"
+
+echo "Building coc.nvim got autocomplete"
+cd $VUNDLE/../coc.nvim
+npm ci
+
+echo "Adding C/C++ autocomplete"
+nvim -c ":CocInstall clangd" -c ":CocCommand clangd.install" -c ":q" -c ":q"
+
