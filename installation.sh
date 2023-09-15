ff1a2368dfd1565a769611c1d77b7ed12e36af48
Beatz748 <pashkamos77@gmail.com>
2023-09-15 03:13:12 +0300

    NeoVim <3

--- installation.sh
+++ installation.sh
@@ -1,4 +1,9 @@
 
+echo "Creating directory for nvim"
+
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/nvim
+
 export VUNDLE="${HOME}/.config/nvim/plugins/Vundle.vim" # You can edit this line, if you already
 							# donwloaded Vundle, set another path.
 echo "Vundle will be downloaded to the $VUNDLE"
@@ -11,3 +16,5 @@ echo "set rtp+=${VUNDLE}" | cat - init.vim > temp && mv temp init.vim
 
 echo "If you are using zshrc, you can type 'v' instead of 'nvim'"
 alias v=nvim >> $HOME/.zshrc
+
+cp coc-settings.json init.vim $HOME/.config/nvim/
